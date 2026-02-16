40d50a8552914d0d16224e5499d1bbdd7553fb37
Ritu Dhanani <ritu.taglineinfo.in>
2026-02-16 14:44:47 +0530

    step3(4):change-password API

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django.contrib.auth import get_user_model
 
 #signup serializer
 class SignupSerializer(serializers.ModelSerializer):
@@ -27,3 +28,20 @@ class SignupSerializer(serializers.ModelSerializer):
 #login serializer
 class LoginSerializer(TokenObtainPairSerializer):
     username_field = 'email'
+
+# Change Password Serializer
+User = get_user_model()
+class ChangePasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("Old password is incorrect")
+        return value
+
+    def validate_new_password(self, value):
+        if len(value) < 6:
+            raise serializers.ValidationError("Password must be at least 6 characters")
+        return value
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import SignupAPIView, LoginAPIView, RefreshTokenAPIView
+from .views import SignupAPIView, LoginAPIView, RefreshTokenAPIView, ChangePasswordAPIView
 
 urlpatterns = [
     path('signup/', SignupAPIView.as_view(), name='signup'),
     path('login/', LoginAPIView.as_view(), name='login'),
     path('token/refresh/', RefreshTokenAPIView.as_view(), name='token_refresh'),
+    path('change-password/', ChangePasswordAPIView.as_view(), name='change_password'),
 ]
