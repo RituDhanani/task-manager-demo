7063765890a667718219ed89f468c85c325ffe62
Ritu Dhanani <ritu.taglineinfo.in>
2026-02-25 14:57:22 +0530

    update with static file.all features has been completed.

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -20,7 +20,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.urls import re_path
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -42,5 +43,5 @@ urlpatterns = [
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
-
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
