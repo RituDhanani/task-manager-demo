7dc718cc7a2756f177b6bd77f64ccf65fee2ce00
Ritu Dhanani <ritu.taglineinfo.in>
2026-02-26 15:57:01 +0530

    create websocket consumer

--- task_manager/routing.py
+++ task_manager/routing.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from chat.routing import websocket_urlpatterns as chat_ws_urls
 
 websocket_urlpatterns = [
-    # Define your WebSocket URL patterns here if needed
+    *chat_ws_urls,
 ]
