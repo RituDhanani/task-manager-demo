d8c080f8cfbcce21c18f214c4a5ca287c985e4ef
Ritu Dhanani <ritu.taglineinfo.in>
2026-02-26 14:08:12 +0530

    step3 update asgi and integrate websocket routing.

--- task_manager/asgi.py
+++ task_manager/asgi.py
@@ -1,16 +1,19 @@
-"""
-ASGI config for task_manager project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import task_manager.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(
+            URLRouter(task_manager.routing.websocket_urlpatterns )
+        ),
+    }
+)
+
