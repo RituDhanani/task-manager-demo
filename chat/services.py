f32f4fc685022e2c1dfc019d07692cd7dc08a003
Ritu Dhanani <ritu.taglineinfo.in>
2026-02-27 14:55:46 +0530

    update step9

--- chat/services.py
+++ chat/services.py
@@ -1,8 +1,24 @@
-from chat.models import ChatRoom
+from chat.models import ChatRoom, Message
 from tasks.models import Task
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+def create_message(*, room_id: int, user_id: int, content: str) -> Message:
+  
+    room = ChatRoom.objects.get(id=room_id)
+    user = User.objects.get(id=user_id)
+
+    return Message.objects.create(
+        room=room,
+        sender=user,
+        content=content,
+    )
 
 
 def user_can_join_room(*, user, room_id: int) -> bool:
+
     if user.is_superuser:
         return True
 
